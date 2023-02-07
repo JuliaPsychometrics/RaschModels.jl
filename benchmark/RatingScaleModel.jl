28fd34033a90a755b559b70b0018f31be931b49c
p-gw <p.gewessler@gmx.at>
2023-02-07 15:01:13 +0100

    reduce allocations for polytomous models

--- benchmark/RatingScaleModel.jl
+++ benchmark/RatingScaleModel.jl
@@ -14,15 +14,24 @@ mle = fit(RatingScaleModel, data, MLE())
 @benchmark irf($mle, $randn(), $(rand(1:n_items)), $(rand(1:n_categories)))
 @benchmark iif($mle, $randn(), $(rand(1:n_items)), $(rand(1:n_categories)))
 
+@benchmark expected_score($mle, $randn(), $(rand(1:n_items)))
+@benchmark expected_score($mle, $randn(), $(rand(1:n_items, 3)))
+@benchmark expected_score($mle, $randn())
 
-iif(mle, 0.0, 1, 1)
-@profview_allocs iif(mle, 0.0, 1, 1) sample_rate = 1
+@benchmark information($mle, $randn(), $(rand(1:n_items)))
+@benchmark information($mle, $randn(), $(rand(1:n_items, 3)))
+@benchmark information($mle, $randn())
 
 # SamplingEstimate
 mcmc = fit(RatingScaleModel, data, NUTS(), n_iter)
 
-irf(mcmc, 0.0, 1)
+@benchmark irf($mcmc, $randn(), $(rand(1:n_items)), $(rand(1:n_categories)))
+@benchmark iif($mcmc, $randn(), $(rand(1:n_items)), $(rand(1:n_categories)))
 
-@benchmark irf($mcmc, $randn(), $(rand(1:n_items)))
-@benchmark irf($mcmc, $randn(), $(rand(1:n_items)))
+@benchmark expected_score($mcmc, $randn(), $(rand(1:n_items)))
+@benchmark expected_score($mcmc, $randn(), $(rand(1:n_items, 3)))
+@benchmark expected_score($mcmc, $randn())
 
+@benchmark information($mcmc, $randn(), $(rand(1:n_items)))
+@benchmark information($mcmc, $randn(), $(rand(1:n_items, 3)))
+@benchmark information($mcmc, $randn())
--- src/models/RatingScaleModel.jl
+++ src/models/RatingScaleModel.jl
@@ -11,13 +11,13 @@ struct RatingScaleModel{ET<:EstimationType,DT<:AbstractMatrix,PT} <:
     parnames_tau::Vector{Symbol}
 end
 
-function _get_item_thresholds(
-    model::RatingScaleModel{ET,DT,PT},
-    i,
-)::Matrix{Float64} where {ET,DT,PT<:Chains}
+function _get_item_thresholds(model::RatingScaleModel{ET,DT,PT}, i) where {ET,DT,PT<:Chains}
     threshold_names = model.parnames_tau
-    thresholds = Array(model.pars[threshold_names])
-    return thresholds
+    thresholds = view(model.pars.value, var = threshold_names)
+    n_iter, n_pars, n_chains = size(thresholds)
+    thresholds_permuted = permutedims(thresholds, (1, 3, 2))
+    threshold_mat = Matrix(reshape(thresholds_permuted, n_iter * n_chains, n_pars))
+    return threshold_mat
 end
 
 function _get_item_thresholds(
